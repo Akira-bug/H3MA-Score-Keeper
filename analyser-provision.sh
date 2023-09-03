d7c159c4c46b7e1ce321405c73850caa9bf5da02
Luke Webb <lukewebb2000@gmail.com>
2023-09-03 14:55:47 +1200

    Working 3 VM applicatioN! - Third "analsyer" VM runs analysis python scripts on boot. - You can update the scores based on match data for any fencer by running the analyseAll.py file.

--- analyser-provision.sh
+++ analyser-provision.sh
@@ -5,7 +5,9 @@ sudo apt-get update
 
 # Install Python and data analysis libraries
 sudo apt-get install -y python3-pip
-pip3 install pandas numpy matplotlib scikit-learn jupyter  mysql-connector-python
+pip3 install pandas numpy matplotlib scikit-learn  mysql-connector-python
 
-# Start Jupyter Notebook (optional)
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root
+cd /vagrant/analysis
+
+python3 analyseAll.py
+
